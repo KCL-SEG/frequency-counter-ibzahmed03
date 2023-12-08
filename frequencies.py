af15993524d2b27778f0cc0b014543e0cfe2201d
ibzahmed03 <iahmed1625@gmail.com>
2023-12-08 15:30:00 +0000

    completed task

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+
+        key = str(item) 
+        frequencies[key] = frequencies.get(key, 0) + 1
+
     return frequencies
